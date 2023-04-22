ab3a3cdd74e23b766cb258e188ed08c74db8d27b
S. Chang <78457278+schang-chapman@users.noreply.github.com>
2023-04-22 11:02:59 -0700

    jank navbar

---
+++ frontend/src/pages/login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const Login = () => {
+  return (
+    <div>
+      <h1>PLACEHOLDER INSERT LOGIN PAGE HERE</h1>
+    </div>
+  );
+};
+  
+export default Login;
---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+  
+const SignUp = () => {
+  return (
+    <div>
+      <h1>PLACEHOLDER INSERT SIGNUP PAGE HERE</h1>
+    </div>
+  );
+};
+  
+export default SignUp;
